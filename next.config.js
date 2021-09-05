abb04fb54c8f249f8ef74c6c18611b87996cb8c4
Naji Astier <naji.astier@algoan.com>
2021-09-05 17:23:29 +0200

    feat: add ability to configure default locale

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { NodeConfigTSPlugin } = require('node-config-ts/webpack');
+const { config } = require('node-config-ts');
+
+const supportedLocales = ['en', 'fr'];
+let defaultLocale = 'en';
+
+if (supportedLocales.includes(config.defaultLocale)) {
+  defaultLocale = config.defaultLocale;
+}
+
+console.log(`Default locale: ${defaultLocale}`);
 
 /** @type {import('next').NextConfig} */
 module.exports = {
@@ -7,7 +18,7 @@ module.exports = {
     return NodeConfigTSPlugin(config);
   },
   i18n: {
-    locales: ['en-GB', 'fr'],
-    defaultLocale: 'en-GB',
+    locales: supportedLocales,
+    defaultLocale,
   },
 };
