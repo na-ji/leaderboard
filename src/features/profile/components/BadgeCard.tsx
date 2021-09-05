a998e619f34beb8c38a32b4f582538a0995611b0
Naji Astier <naji.astier@algoan.com>
2021-09-05 15:45:58 +0200

    feat(profile): add medals translations

--- src/features/profile/components/BadgeCard.tsx
+++ src/features/profile/components/BadgeCard.tsx
@@ -1,20 +1,26 @@
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
+import { useIntl } from 'react-intl';
 
 import { Badge } from '@/types/model';
 import { BadgeIcon } from '@/features/profile/components/BadgeIcon';
+import { badgeTranslations } from '@/features/profile/utils/badgeTranslations';
 
 interface BadgeProps {
   badge: Badge;
   value: number;
 }
 
-export const BadgeCard = ({ badge, value }: BadgeProps): JSX.Element => (
-  <Card sx={{ minWidth: 275 }}>
-    <CardHeader
-      avatar={<BadgeIcon badge={badge} value={value} />}
-      title={badge}
-      subheader={value && value.toLocaleString()}
-    />
-  </Card>
-);
+export const BadgeCard = ({ badge, value }: BadgeProps): JSX.Element => {
+  const intl = useIntl();
+
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardHeader
+        avatar={<BadgeIcon badge={badge} value={value} />}
+        title={intl.formatMessage(badgeTranslations[badge])}
+        subheader={value && intl.formatNumber(value)}
+      />
+    </Card>
+  );
+};
---
+++ src/features/profile/utils/badgeTranslations.ts
@@ -0,0 +1,326 @@
+import { defineMessages } from 'react-intl';
+
+import { Badge } from '@/types/model';
+
+export const badgeTranslations = defineMessages({
+  [Badge.KM_WALKED]: {
+    id: 'badge_km_walked',
+    defaultMessage: 'Km walked',
+    description: 'Badge KM_WALKED',
+  },
+  [Badge.CAUGHT_POKEMON]: {
+    id: 'badge_caught_pokemon',
+    defaultMessage: 'Caught pokemon',
+    description: 'Badge CAUGHT_POKEMON',
+  },
+  [Badge.STOPS_SPUN]: {
+    id: 'badge_stops_spun',
+    defaultMessage: 'Stops spun',
+    description: 'Badge STOPS_SPUN',
+  },
+  [Badge.EVOLVED]: {
+    id: 'badge_evolved',
+    defaultMessage: 'Evolved',
+    description: 'Badge EVOLVED',
+  },
+  [Badge.HATCHED]: {
+    id: 'badge_hatched',
+    defaultMessage: 'Hatched',
+    description: 'Badge HATCHED',
+  },
+  [Badge.QUESTS]: {
+    id: 'badge_quests',
+    defaultMessage: 'Quests',
+    description: 'Badge QUESTS',
+  },
+  [Badge.TRADES]: {
+    id: 'badge_trades',
+    defaultMessage: 'Trades',
+    description: 'Badge TRADES',
+  },
+  [Badge.PHOTOBOMBS]: {
+    id: 'badge_photobombs',
+    defaultMessage: 'Photobombs',
+    description: 'Badge PHOTOBOMBS',
+  },
+  [Badge.PURIFIED]: {
+    id: 'badge_purified',
+    defaultMessage: 'Purified',
+    description: 'Badge PURIFIED',
+  },
+  [Badge.GRUNTS_DEFEATED]: {
+    id: 'badge_grunts_defeated',
+    defaultMessage: 'Grunts defeated',
+    description: 'Badge GRUNTS_DEFEATED',
+  },
+  [Badge.GYM_BATTLES_WON]: {
+    id: 'badge_gym_battles_won',
+    defaultMessage: 'Gym battles won',
+    description: 'Badge GYM_BATTLES_WON',
+  },
+  [Badge.NORMAL_RAIDS_WON]: {
+    id: 'badge_normal_raids_won',
+    defaultMessage: 'Normal raids won',
+    description: 'Badge NORMAL_RAIDS_WON',
+  },
+  [Badge.LEGENDARY_RAIDS_WON]: {
+    id: 'badge_legendary_raids_won',
+    defaultMessage: 'Legendary raids won',
+    description: 'Badge LEGENDARY_RAIDS_WON',
+  },
+  [Badge.TRAININGS_WON]: {
+    id: 'badge_trainings_won',
+    defaultMessage: 'Trainings won',
+    description: 'Badge TRAININGS_WON',
+  },
+  [Badge.BERRIES_FED]: {
+    id: 'badge_berries_fed',
+    defaultMessage: 'Berries fed',
+    description: 'Badge BERRIES_FED',
+  },
+  [Badge.HOURS_DEFENDED]: {
+    id: 'badge_hours_defended',
+    defaultMessage: 'Hours defended',
+    description: 'Badge HOURS_DEFENDED',
+  },
+  [Badge.BEST_FRIENDS]: {
+    id: 'badge_best_friends',
+    defaultMessage: 'Best friends',
+    description: 'Badge BEST_FRIENDS',
+  },
+  [Badge.BEST_BUDDIES]: {
+    id: 'badge_best_buddies',
+    defaultMessage: 'Best buddies',
+    description: 'Badge BEST_BUDDIES',
+  },
+  [Badge.GIOVANNI_DEFEATED]: {
+    id: 'badge_giovanni_defeated',
+    defaultMessage: 'Giovanni defeated',
+    description: 'Badge GIOVANNI_DEFEATED',
+  },
+  [Badge.MEGA_EVOS]: {
+    id: 'badge_mega_evos',
+    defaultMessage: 'Mega evolutions',
+    description: 'Badge MEGA_EVOS',
+  },
+  [Badge.COLLECTIONS_DONE]: {
+    id: 'badge_collections_done',
+    defaultMessage: 'Collections done',
+    description: 'Badge COLLECTIONS_DONE',
+  },
+  [Badge.UNIQUE_STOPS_SPUN]: {
+    id: 'badge_unique_stops_spun',
+    defaultMessage: 'Unique stops spun',
+    description: 'Badge UNIQUE_STOPS_SPUN',
+  },
+  [Badge.UNIQUE_MEGA_EVOS]: {
+    id: 'badge_unique_mega_evos',
+    defaultMessage: 'Unique mega evolutions',
+    description: 'Badge UNIQUE_MEGA_EVOS',
+  },
+  [Badge.UNIQUE_RAID_BOSSES]: {
+    id: 'badge_unique_raid_bosses',
+    defaultMessage: 'Unique raid bosses',
+    description: 'Badge UNIQUE_RAID_BOSSES',
+  },
+  [Badge.UNIQUE_UNOWN]: {
+    id: 'badge_unique_unown',
+    defaultMessage: 'Unique Unown',
+    description: 'Badge UNIQUE_UNOWN',
+  },
+  [Badge['7_DAY_STREAKS']]: {
+    id: 'badge_7_day_streaks',
+    defaultMessage: '7 day streaks',
+    description: 'Badge 7_DAY_STREAKS',
+  },
+  [Badge.TRADE_KM]: {
+    id: 'badge_trade_km',
+    defaultMessage: 'Trade km',
+    description: 'Badge TRADE_KM',
+  },
+  [Badge.RAIDS_WITH_FRIENDS]: {
+    id: 'badge_raids_with_friends',
+    defaultMessage: 'Raids with friends',
+    description: 'Badge RAIDS_WITH_FRIENDS',
+  },
+  [Badge.CAUGHT_AT_LURE]: {
+    id: 'badge_caught_at_lure',
+    defaultMessage: 'Caught at lure',
+    description: 'Badge CAUGHT_AT_LURE',
+  },
+  [Badge.WAYFARER_AGREEMENTS]: {
+    id: 'badge_wayfarer_agreements',
+    defaultMessage: 'Wayfarer agreements',
+    description: 'Badge WAYFARER_AGREEMENTS',
+  },
+  [Badge.TRAINERS_REFERRED]: {
+    id: 'badge_trainers_referred',
+    defaultMessage: 'Trainers referred',
+    description: 'Badge TRAINERS_REFERRED',
+  },
+  [Badge.RAID_ACHIEVEMENTS]: {
+    id: 'badge_raid_achievements',
+    defaultMessage: 'Raid achievements',
+    description: 'Badge RAID_ACHIEVEMENTS',
+  },
+  [Badge.XL_KARPS]: {
+    id: 'badge_xl_karps',
+    defaultMessage: 'XL Magikarp',
+    description: 'Badge XL_KARPS',
+  },
+  [Badge.XS_RATS]: {
+    id: 'badge_xs_rats',
+    defaultMessage: 'XS Rattata',
+    description: 'Badge XS_RATS',
+  },
+  [Badge.PIKACHU_CAUGHT]: {
+    id: 'badge_pikachu_caught',
+    defaultMessage: 'Pikachu caught',
+    description: 'Badge PIKACHU_CAUGHT',
+  },
+  [Badge.LEAGUE_GREAT_WON]: {
+    id: 'badge_league_great_won',
+    defaultMessage: 'Great league won',
+    description: 'Badge LEAGUE_GREAT_WON',
+  },
+  [Badge.LEAGUE_ULTRA_WON]: {
+    id: 'badge_league_ultra_won',
+    defaultMessage: 'Ultra league won',
+    description: 'Badge LEAGUE_ULTRA_WON',
+  },
+  [Badge.LEAGUE_MASTER_WON]: {
+    id: 'badge_league_master_won',
+    defaultMessage: 'Master league won',
+    description: 'Badge LEAGUE_MASTER_WON',
+  },
+  [Badge.DEX_GEN_1]: {
+    id: 'badge_dex_gen_1',
+    defaultMessage: 'Gen 1',
+    description: 'Badge DEX_GEN_1',
+  },
+  [Badge.DEX_GEN_2]: {
+    id: 'badge_dex_gen_2',
+    defaultMessage: 'Gen 2',
+    description: 'Badge DEX_GEN_2',
+  },
+  [Badge.DEX_GEN_3]: {
+    id: 'badge_dex_gen_3',
+    defaultMessage: 'Gen 3',
+    description: 'Badge DEX_GEN_3',
+  },
+  [Badge.DEX_GEN_4]: {
+    id: 'badge_dex_gen_4',
+    defaultMessage: 'Gen 4',
+    description: 'Badge DEX_GEN_4',
+  },
+  [Badge.DEX_GEN_5]: {
+    id: 'badge_dex_gen_5',
+    defaultMessage: 'Gen 5',
+    description: 'Badge DEX_GEN_5',
+  },
+  [Badge.DEX_GEN_6]: {
+    id: 'badge_dex_gen_6',
+    defaultMessage: 'Gen 6',
+    description: 'Badge DEX_GEN_6',
+  },
+  [Badge.DEX_GEN_7]: {
+    id: 'badge_dex_gen_7',
+    defaultMessage: 'Dex gen 7',
+    description: 'Badge DEX_GEN_7',
+  },
+  [Badge.DEX_GEN_8]: {
+    id: 'badge_dex_gen_8',
+    defaultMessage: 'Dex gen 8',
+    description: 'Badge DEX_GEN_8',
+  },
+  [Badge.CAUGHT_NORMAL]: {
+    id: 'badge_caught_normal',
+    defaultMessage: 'Normal',
+    description: 'Badge CAUGHT_NORMAL',
+  },
+  [Badge.CAUGHT_FIGHTING]: {
+    id: 'badge_caught_fighting',
+    defaultMessage: 'Fighting',
+    description: 'Badge CAUGHT_FIGHTING',
+  },
+  [Badge.CAUGHT_FLYING]: {
+    id: 'badge_caught_flying',
+    defaultMessage: 'Flying',
+    description: 'Badge CAUGHT_FLYING',
+  },
+  [Badge.CAUGHT_POISON]: {
+    id: 'badge_caught_poison',
+    defaultMessage: 'Poison',
+    description: 'Badge CAUGHT_POISON',
+  },
+  [Badge.CAUGHT_GROUND]: {
+    id: 'badge_caught_ground',
+    defaultMessage: 'Ground',
+    description: 'Badge CAUGHT_GROUND',
+  },
+  [Badge.CAUGHT_ROCK]: {
+    id: 'badge_caught_rock',
+    defaultMessage: 'Rock',
+    description: 'Badge CAUGHT_ROCK',
+  },
+  [Badge.CAUGHT_BUG]: {
+    id: 'badge_caught_bug',
+    defaultMessage: 'Bug',
+    description: 'Badge CAUGHT_BUG',
+  },
+  [Badge.CAUGHT_GHOST]: {
+    id: 'badge_caught_ghost',
+    defaultMessage: 'Ghost',
+    description: 'Badge CAUGHT_GHOST',
+  },
+  [Badge.CAUGHT_STEEL]: {
+    id: 'badge_caught_steel',
+    defaultMessage: 'Steel',
+    description: 'Badge CAUGHT_STEEL',
+  },
+  [Badge.CAUGHT_FIRE]: {
+    id: 'badge_caught_fire',
+    defaultMessage: 'Fire',
+    description: 'Badge CAUGHT_FIRE',
+  },
+  [Badge.CAUGHT_WATER]: {
+    id: 'badge_caught_water',
+    defaultMessage: 'Water',
+    description: 'Badge CAUGHT_WATER',
+  },
+  [Badge.CAUGHT_GRASS]: {
+    id: 'badge_caught_grass',
+    defaultMessage: 'Grass',
+    description: 'Badge CAUGHT_GRASS',
+  },
+  [Badge.CAUGHT_ELECTRIC]: {
+    id: 'badge_caught_electric',
+    defaultMessage: 'Electric',
+    description: 'Badge CAUGHT_ELECTRIC',
+  },
+  [Badge.CAUGHT_PSYCHIC]: {
+    id: 'badge_caught_psychic',
+    defaultMessage: 'Psychic',
+    description: 'Badge CAUGHT_PSYCHIC',
+  },
+  [Badge.CAUGHT_ICE]: {
+    id: 'badge_caught_ice',
+    defaultMessage: 'Ice',
+    description: 'Badge CAUGHT_ICE',
+  },
+  [Badge.CAUGHT_DRAGON]: {
+    id: 'badge_caught_dragon',
+    defaultMessage: 'Dragon',
+    description: 'Badge CAUGHT_DRAGON',
+  },
+  [Badge.CAUGHT_DARK]: {
+    id: 'badge_caught_dark',
+    defaultMessage: 'Dark',
+    description: 'Badge CAUGHT_DARK',
+  },
+  [Badge.CAUGHT_FAIRY]: {
+    id: 'badge_caught_fairy',
+    defaultMessage: 'Fairy',
+    description: 'Badge CAUGHT_FAIRY',
+  },
+});
