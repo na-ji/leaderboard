912de7cbbfca08a733eb9172f529aa8847b1de4b
Naji Astier <naji.astier@algoan.com>
2021-09-05 16:08:06 +0200

    feat(leaderboard): add column header translations

--- src/features/leaderboard/components/Leaderboard.tsx
+++ src/features/leaderboard/components/Leaderboard.tsx
@@ -7,15 +7,19 @@ import { useEffect, useState } from 'react';
 import { Team, Trainer } from '@/types/model';
 import { getLightTeamColor } from '@/utils/team-colors';
 import { SupportedLocale } from '@/utils/i18n';
+import { defineMessages, useIntl } from 'react-intl';
 
 interface LeaderboardProps {
   trainers: Trainer[];
 }
 
-const columns: GridColDef[] = [
+const columns: Array<
+  GridColDef & {
+    field: Extract<keyof Trainer, 'trainer_id' | 'name' | 'level' | 'xp' | 'battles_won' | 'caught_pokemon'>;
+  }
+> = [
   {
     field: 'trainer_id',
-    headerName: 'Rank',
     width: 90,
     sortable: false,
     type: 'number',
@@ -26,34 +30,29 @@ const columns: GridColDef[] = [
   {
     field: 'name',
     flex: 1,
-    headerName: 'Trainer',
     minWidth: 150,
   },
   {
     field: 'level',
     flex: 1,
-    headerName: 'Level',
-    minWidth: 120,
+    minWidth: 130,
     type: 'number',
   },
   {
     field: 'xp',
     flex: 1,
-    headerName: 'XP',
     minWidth: 120,
     type: 'number',
   },
   {
     field: 'battles_won',
     flex: 1,
-    headerName: 'Battles won',
-    minWidth: 160,
+    minWidth: 200,
     type: 'number',
   },
   {
     field: 'caught_pokemon',
     flex: 1,
-    headerName: 'Caught Pokémon',
     minWidth: 200,
     type: 'number',
   },
@@ -75,8 +74,42 @@ const ColoredTeamRowsContainer = styled(Box)`
   }
 `;
 
+export const columnHeaderTranslations = defineMessages({
+  trainer_id: {
+    id: 'leaderboard_header.rank',
+    defaultMessage: 'Rank',
+    description: 'Leaderboard rank column header',
+  },
+  name: {
+    id: 'leaderboard_header.name',
+    defaultMessage: 'Trainer',
+    description: 'Leaderboard trainer column header',
+  },
+  level: {
+    id: 'leaderboard_header.level',
+    defaultMessage: 'Level',
+    description: 'Leaderboard level column header',
+  },
+  xp: {
+    id: 'leaderboard_header.xp',
+    defaultMessage: 'XP',
+    description: 'Leaderboard xp column header',
+  },
+  battles_won: {
+    id: 'leaderboard_header.battles_won',
+    defaultMessage: 'Battles won',
+    description: 'Leaderboard battles won column header',
+  },
+  caught_pokemon: {
+    id: 'leaderboard_header.caught_pokemon',
+    defaultMessage: 'Pokémon caught',
+    description: 'Leaderboard caught pokemon column header',
+  },
+});
+
 export const Leaderboard = ({ trainers }: LeaderboardProps): JSX.Element => {
   const router = useRouter();
+  const intl = useIntl();
 
   const { locale } = router;
   let localeText: Partial<GridLocaleText> = {};
@@ -95,7 +128,10 @@ export const Leaderboard = ({ trainers }: LeaderboardProps): JSX.Element => {
     <ColoredTeamRowsContainer>
       <DataGrid
         autoHeight
-        columns={columns}
+        columns={columns.map((column) => ({
+          ...column,
+          headerName: intl.formatMessage(columnHeaderTranslations[column.field]),
+        }))}
         getRowClassName={(params) => `team-${params.row.team}`}
         getRowId={(row) => row.trainer_id}
         hideFooter
