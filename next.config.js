77b706b8234132824f23eb4648b465c4e953b572
Naji Astier <naji.astier@algoan.com>
2021-09-12 15:49:53 +0200

    ci: add computation of bundle size

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { NodeConfigTSPlugin } = require('node-config-ts/webpack');
 const { config } = require('node-config-ts');
+const { NodeConfigTSPlugin } = require('node-config-ts/webpack');
+const { StatsWriterPlugin } = require('webpack-stats-plugin');
 
 const supportedLocales = ['en', 'fr'];
 let defaultLocale = 'en';
@@ -15,6 +16,19 @@ console.log(`Default locale: ${defaultLocale}`);
 module.exports = {
   reactStrictMode: true,
   webpack: (config) => {
+    config.plugins.push(
+      new StatsWriterPlugin({
+        filename: 'stats.json',
+        stats: {
+          context: './src', // optional, will improve readability of the paths
+          assets: true,
+          entrypoints: true,
+          chunks: true,
+          modules: true,
+        },
+      }),
+    );
+
     return NodeConfigTSPlugin(config);
   },
   i18n: {
