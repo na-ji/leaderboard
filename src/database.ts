305ec1c32e61b148fb70bb69dca183f437233223
Naji Astier <naji.astier@algoan.com>
2021-09-18 17:52:25 +0200

    feat: add possibility to configure database port

--- src/database.ts
+++ src/database.ts
@@ -1,13 +1,14 @@
 import { config } from 'node-config-ts';
 import mysql from 'mysql2/promise';
 
-const { host, user, password, name: database } = config.database;
+const { host, user, password, name: database, port } = config.database;
 
 export const pool = mysql.createPool({
   host,
   user,
   password,
   database,
+  port: port ?? 3306,
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0,
