4cc8bdbb04bb4c61303f7fa31f1a4c5aeee4e2f6
Naji Astier <naji.astier@algoan.com>
2021-10-10 22:49:16 +0200

    fix: display log timestamp in server timezone

--- server/logger.ts
+++ server/logger.ts
@@ -2,6 +2,7 @@ import { Logger } from 'tslog';
 
 export const logger: Logger = new Logger({
   dateTimePattern: 'year-month-day hour:minute:second',
+  dateTimeTimezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
   displayFilePath: 'hidden',
   displayFunctionName: false,
   overwriteConsole: true,
