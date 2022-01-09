fb178b7b6c1c21ab533a16bd2538a8490ba7a41f
Naji Astier <naji.astier@algoan.com>
2022-01-09 22:45:51 +0100

    refactor(database): prevent port type to be string

--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,17 @@
 import { config } from 'node-config-ts';
 import mysql from 'mysql2/promise';
 
-const { host, user, password, leaderboardDatabase: database, port } = config.database;
+const { host, user, password, madDatabase, leaderboardDatabase: database } = config.database;
+const port: number = parseInt(`${config.database.port}`) ?? 3306;
 
-export const leaderboardConnectionString = `mysql://${user}:${password}@${host}:${port ?? 3306}/${database}`;
+export const leaderboardConnectionString = `mysql://${user}:${password}@${host}:${port}/${database}`;
 
 export const pool = mysql.createPool({
-  host: config.database.host,
-  user: config.database.user,
-  password: config.database.password,
-  database: config.database.madDatabase,
-  port: config.database.port ?? 3306,
+  host,
+  user,
+  password,
+  database: madDatabase,
+  port,
   waitForConnections: true,
   connectionLimit: 3,
   queueLimit: 0,
