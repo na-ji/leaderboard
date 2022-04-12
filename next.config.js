2174fa3ef1d9c948263d1d90606feb47955110fe
Naji Astier <naji.astier@algoan.com>
2022-04-12 19:28:38 +0200

    chore: upgrade tooling packages

--- next.config.js
+++ next.config.js
@@ -52,6 +52,8 @@ module.exports = withTM({
   experimental: {
     cpus: 1,
     workerThreads: 1,
+  },
+  compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true,
   },
