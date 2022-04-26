faa904d27a1e996fe814bf0baeb63b62d7c8a8e0
Naji Astier <naji.astier@algoan.com>
2022-04-26 18:33:53 +0200

    refactor(tab): fix spacing in level 1 tab

--- src/components/Tab/List.tsx
+++ src/components/Tab/List.tsx
@@ -4,7 +4,10 @@ export type TabProps = Parameters<typeof HeadlessTab.Group>[0] & { level?: 1 | 2
 
 export const List = ({ children, level, className, ...props }: TabProps): JSX.Element => {
   return (
-    <HeadlessTab.List className={`${level === 1 ? 'border-b border-grey-20 ' : ''}${className}`} {...props}>
+    <HeadlessTab.List
+      className={`flex pb-3 lg:pb-5 items-center ${level === 1 ? ' border-b border-grey-20 ' : ''} ${className || ''}`}
+      {...props}
+    >
       {children}
     </HeadlessTab.List>
   );
--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -8,13 +8,13 @@ export const Tab = ({ level = 1, children, ...props }: TabProps): JSX.Element =>
     <HeadlessTab
       className={({ selected }) => {
         return clsx({
-          'px-3.5 title-3 h-5 focus:outline-none text-grey-70 relative': true,
+          'px-3.5 title-3 focus:outline-none text-grey-70 relative': true,
           'before:content-[attr(data-text)] before:h-0 before:invisible before:font-semibold before:overflow-hidden before:pointer-events-none before:block':
             true,
-          'first:pl-0 hover:text-black disabled:text-[#BCC3D7] lg:title-2 pb-5 lg:pb-7.5': level === 1,
-          'after:content-[attr(data-text)] after:text-transparent after:h-0.5 after:bg-gradient after:block after:absolute after:bottom-[-1px] after:rounded-md':
+          'first:pl-0 hover:text-black disabled:text-[#BCC3D7] lg:title-2 my-3 lg:my-2.5 h-4 lg:h-5': level === 1,
+          'after:content-[attr(data-text)] after:text-transparent after:h-0.5 after:bg-gradient after:block after:absolute after:bottom-[-25px] lg:after:bottom-[-31px] after:rounded-md':
             level === 1 && selected,
-          'text-black lg:font-semibold': level === 1 && selected,
+          '!text-black lg:font-semibold': level === 1 && selected,
           'bg-blue-20 lg:h-10 h-9 rounded mr-1.5 lg:mr-3 last:mr-0': level === 2,
           'hover:bg-[rgba(68,121,254,0.1)] hover:text-blue-100 hover:font-semibold disabled:text-[#BCC3D7] disabled:hover:font-normal disabled:bg-[#F2F4F9]':
             level === 2,
