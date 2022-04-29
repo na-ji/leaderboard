5764f90c3dcc2c85059f5485055b0e841d65286e
Naji Astier <naji.astier@algoan.com>
2022-04-29 18:28:37 +0200

    refactor(profile): add team based trainer avatar

--- src/features/profile/components/TrainerCard.tsx
+++ src/features/profile/components/TrainerCard.tsx
@@ -3,8 +3,7 @@ import { FormattedMessage } from 'react-intl';
 import { memo } from 'react';
 
 import { Card } from '@/components/Card';
-import { Trainer } from '@/types';
-import avatarPicture from '../../../../public/avatar.png';
+import { Team, Trainer } from '@/types';
 import { ProgressBar } from '@/features/profile/components/ProgressBar';
 
 interface TrainerCardProps {
@@ -12,11 +11,26 @@ interface TrainerCardProps {
   trainer: Trainer;
 }
 
+const avatarBackgroundColors = {
+  [Team.MYSTIC]: '#c1effc',
+  [Team.VALOR]: '#fdd8d8',
+  [Team.INSTINCT]: '#fdf4b6',
+};
+
 export const TrainerCard = memo(({ className, trainer }: TrainerCardProps): JSX.Element => {
   return (
     <Card className={`flex h-[102px] lg:h-[172px] p-4 lg:px-[30px] lg:py-[26px] ${className || ''}`}>
-      <div className="h-[70px] w-[70px] lg:h-[120px] lg:w-[120px]">
-        <NextImage src={avatarPicture} alt="Avatar" width={120} height={120} unoptimized={!!process.env.IS_STORYBOOK} />
+      <div
+        className="h-[70px] w-[70px] lg:h-[120px] lg:w-[120px] rounded-full overflow-hidden"
+        style={{ backgroundColor: avatarBackgroundColors[trainer.team] }}
+      >
+        <NextImage
+          src={`/avatar_${trainer.team}.png`}
+          alt="Avatar"
+          width={120}
+          height={120}
+          unoptimized={!!process.env.IS_STORYBOOK}
+        />
       </div>
       <div className="grow ml-3.5 lg:ml-6 flex flex-col">
         <h3 className="title-2 lg:title-1">{trainer.name}</h3>
