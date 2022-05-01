18248e7e9cc6ef4f07fa693069a13c8c1355ef3b
Naji Astier <naji.astier@algoan.com>
2022-05-01 17:25:06 +0200

    chore(i18n): synchronise language files to match english

---
+++ src/i18n/synchroniseTranslationFile.ts
@@ -0,0 +1,25 @@
+import { resolve } from 'path';
+import { writeFileSync } from 'fs';
+
+import en from './lang/en.json';
+import oldFr from './lang/fr.json';
+
+const synchroniseTranslationFile = (name: 'fr', oldTranslations: typeof en): void => {
+  const newTranslations: Partial<typeof en> = {};
+
+  (Object.keys(en) as Array<keyof typeof en>).forEach((translationId) => {
+    if (translationId in oldTranslations) {
+      newTranslations[translationId] = oldTranslations[translationId];
+    } else {
+      newTranslations[translationId] = en[translationId];
+    }
+  });
+
+  try {
+    writeFileSync(resolve(__dirname, `./lang/${name}.json`), JSON.stringify(newTranslations, null, 2), { flag: 'w+' });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+synchroniseTranslationFile('fr', oldFr);
