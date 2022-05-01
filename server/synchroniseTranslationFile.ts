66480052c679e85c6f71d6848758f0f56f18ade6
Naji Astier <naji.astier@algoan.com>
2022-05-01 22:05:24 +0200

    refactor: move i18n messages sync script to server folder

--- server/synchroniseTranslationFile.ts
+++ server/synchroniseTranslationFile.ts
@@ -1,9 +1,9 @@
 import { resolve } from 'path';
 import { writeFileSync } from 'fs';
 
-import en from './lang/en.json';
-import oldFr from './lang/fr.json';
-import oldDe from './lang/de.json';
+import en from '../src/i18n/lang/en.json';
+import oldFr from '../src/i18n/lang/fr.json';
+import oldDe from '../src/i18n/lang/de.json';
 
 const synchroniseTranslationFile = (name: 'fr' | 'de', oldTranslations: typeof en): void => {
   const newTranslations: Partial<typeof en> = {};
@@ -12,16 +12,19 @@ const synchroniseTranslationFile = (name: 'fr' | 'de', oldTranslations: typeof e
     if (translationId in oldTranslations) {
       newTranslations[translationId] = oldTranslations[translationId];
     } else {
+      console.log(`[${name}] creating new message for "${translationId}"`);
       newTranslations[translationId] = en[translationId];
     }
   });
 
   try {
-    writeFileSync(resolve(__dirname, `./lang/${name}.json`), JSON.stringify(newTranslations, null, 2), { flag: 'w+' });
+    writeFileSync(resolve(__dirname, `../src/i18n/lang/${name}.json`), JSON.stringify(newTranslations, null, 2), {
+      flag: 'w+',
+    });
   } catch (err) {
     console.error(err);
   }
 };
 
-synchroniseTranslationFile('fr', oldFr);
-synchroniseTranslationFile('de', oldDe);
+synchroniseTranslationFile('fr', oldFr as unknown as typeof en);
+synchroniseTranslationFile('de', oldDe as unknown as typeof en);
