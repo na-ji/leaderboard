5dddbc01b4d178ae608f0c74ef8541ff7af0ea72
Naji Astier <naji.astier@algoan.com>
2022-05-02 12:33:53 +0200

    chore(storybook): expose config to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 const { mergeConfig } = require('vite');
 const tsconfigPaths = require('vite-tsconfig-paths').default;
 const EnvironmentPlugin = require('vite-plugin-environment').default;
+const { config: appConfig } = require('node-config-ts');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -18,6 +19,14 @@ module.exports = {
   async viteFinal(config) {
     // return the customized config
     return mergeConfig(config, {
+      resolve: {
+        alias: {
+          'node-config-ts': 'node-config-ts/iso',
+        },
+      },
+      define: {
+        __CONFIG__: appConfig,
+      },
       plugins: [tsconfigPaths(), new EnvironmentPlugin({ IS_STORYBOOK: '1' })],
     });
   },
