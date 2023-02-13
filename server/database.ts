2018657de8e62f796c2a6da160359d5fc2119c7a
Naji Astier <naji.astier@algoan.com>
2023-02-13 09:24:06 +0100

    refactor: add query to migrate history to golbat

--- server/database.ts
+++ server/database.ts
@@ -6,9 +6,9 @@ import { logger } from './logger';
 const schemaCreationQuery = `
     CREATE TABLE IF NOT EXISTS ${config.database.leaderboardDatabase}.pogo_leaderboard_trainer_history
     (
-        \`date\`                  date                                    NOT NULL DEFAULT (curdate()),
-        \`RPL\`                   smallint(6)                             NOT NULL,
-        \`name\`                  varchar(20)                             NOT NULL,
+        \`date\`                  date                    NOT NULL DEFAULT (curdate()),
+        \`RPL\`                   smallint(6)             NOT NULL,
+        \`name\`                  varchar(20)             NOT NULL,
         \`team\`                  tinyint(1) UNSIGNED     DEFAULT NULL,
         \`level\`                 tinyint(2) UNSIGNED     DEFAULT NULL,
         \`xp\`                    bigint(9) UNSIGNED      DEFAULT NULL,
@@ -102,6 +102,88 @@ const schemaUpdateQuery = `
     WHERE friendship_id IS NOT NULL and name <> '';
 `;
 
+const migrateToGolbatQuery = `
+  ALTER TABLE ${config.database.leaderboardDatabase}.pogo_leaderboard_trainer_history 
+    CHANGE \`name\` \`name\`                               VARCHAR(20) NOT NULL,
+    CHANGE \`team\` \`team\`                               TINYINT(1) UNSIGNED DEFAULT NULL,
+    CHANGE \`level\` \`level\`                             TINYINT(2) UNSIGNED DEFAULT NULL,
+    CHANGE \`xp\` \`xp\`                                   BIGINT(9) UNSIGNED DEFAULT NULL,
+    CHANGE \`battles_won\` \`battles_won\`                 BIGINT(8) DEFAULT NULL,
+    CHANGE \`last_seen\` \`last_seen\`                     INT(10) NOT NULL,
+    CHANGE \`km_walked\` \`km_walked\`                     FLOAT UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_pokemon\` \`caught_pokemon\`           INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`trainer_id\` \`friendship_id\`                VARCHAR(100) DEFAULT NULL,
+    CHANGE \`gbl_rank\` \`gbl_rank\`                       TINYINT(2) UNSIGNED DEFAULT NULL,
+    CHANGE \`gbl_rating\` \`gbl_rating\`                   SMALLINT(4) UNSIGNED DEFAULT NULL,
+    CHANGE \`special_badges\` \`event_badges\`             VARCHAR(500) DEFAULT NULL,
+    CHANGE \`stops_spun\` \`stops_spun\`                   INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`evolved\` \`evolved\`                         INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`hatched\` \`hatched\`                         INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`quests\` \`quests\`                           INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`trades\` \`trades\`                           INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`photobombs\` \`photobombs\`                   INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`purified\` \`purified\`                       INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`grunts_defeated\` \`grunts_defeated\`         INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`gym_battles_won\` \`gym_battles_won\`         INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`normal_raids_won\` \`normal_raids_won\`       INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`legendary_raids_won\` \`legendary_raids_won\` INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`trainings_won\` \`trainings_won\`             INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`berries_fed\` \`berries_fed\`                 INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`hours_defended\` \`hours_defended\`           INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`best_friends\` \`best_friends\`               INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`best_buddies\` \`best_buddies\`               INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`giovanni_defeated\` \`giovanni_defeated\`     SMALLINT(3) UNSIGNED DEFAULT NULL,
+    CHANGE \`mega_evos\` \`mega_evos\`                     INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`collections_done\` \`collections_done\`       SMALLINT(3) UNSIGNED DEFAULT NULL,
+    ADD \`vivillon\`                                       TINYINT(2) UNSIGNED DEFAULT NULL,
+    CHANGE \`unique_stops_spun\` \`unique_stops_spun\`     INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`unique_mega_evos\` \`unique_mega_evos\`       INT(3) UNSIGNED DEFAULT NULL,
+    CHANGE \`unique_raid_bosses\` \`unique_raid_bosses\`   INT(4) UNSIGNED DEFAULT NULL,
+    CHANGE \`unique_unown\` \`unique_unown\`               TINYINT(2) UNSIGNED DEFAULT NULL,
+    CHANGE \`7_day_streaks\` \`seven_day_streaks\`         INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`trade_km\` \`trade_km\`                       BIGINT(8) DEFAULT NULL,
+    CHANGE \`raids_with_friends\` \`raids_with_friends\`   INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_at_lure\` \`caught_at_lure\`           INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`wayfarer_agreements\` \`wayfarer_agreements\` INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`trainers_referred\` \`trainers_referred\`     INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`raid_achievements\` \`raid_achievements\`     INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`xl_karps\` \`xl_karps\`                       INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`xs_rats\` \`xs_rats\`                         INT(6) UNSIGNED DEFAULT NULL,
+    ADD \`tiny_pokemon_caught\`                            INT(6) UNSIGNED DEFAULT NULL,
+    ADD \`jumbo_pokemon_caught\`                           INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`pikachu_caught\` \`pikachu_caught\`           INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`league_great_won\` \`league_great_won\`       INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`league_ultra_won\` \`league_ultra_won\`       INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`league_master_won\` \`league_master_won\`     INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`dex_gen1\` \`dex_gen1\`                       TINYINT(3) UNSIGNED DEFAULT NULL,
+    CHANGE \`dex_gen2\` \`dex_gen2\`                       TINYINT(3) UNSIGNED DEFAULT NULL,
+    CHANGE \`dex_gen3\` \`dex_gen3\`                       TINYINT(3) UNSIGNED DEFAULT NULL,
+    CHANGE \`dex_gen4\` \`dex_gen4\`                       TINYINT(3) UNSIGNED DEFAULT NULL,
+    CHANGE \`dex_gen5\` \`dex_gen5\`                       TINYINT(3) UNSIGNED DEFAULT NULL,
+    CHANGE \`dex_gen6\` \`dex_gen6\`                       TINYINT(3) UNSIGNED DEFAULT NULL,
+    CHANGE \`dex_gen7\` \`dex_gen7\`                       TINYINT(3) UNSIGNED DEFAULT NULL,
+    CHANGE \`dex_gen8\` \`dex_gen8\`                       TINYINT(3) UNSIGNED DEFAULT NULL,
+    ADD \`dex_gen8a\`                                      TINYINT(3) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_normal\` \`caught_normal\`             INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_fighting\` \`caught_fighting\`         INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_flying\` \`caught_flying\`             INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_poison\` \`caught_poison\`             INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_ground\` \`caught_ground\`             INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_rock\` \`caught_rock\`                 INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_bug\` \`caught_bug\`                   INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_ghost\` \`caught_ghost\`               INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_steel\` \`caught_steel\`               INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_fire\` \`caught_fire\`                 INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_water\` \`caught_water\`               INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_grass\` \`caught_grass\`               INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_electric\` \`caught_electric\`         INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_psychic\` \`caught_psychic\`           INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_ice\` \`caught_ice\`                   INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_dragon\` \`caught_dragon\`             INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_dark\` \`caught_dark\`                 INT(6) UNSIGNED DEFAULT NULL,
+    CHANGE \`caught_fairy\` \`caught_fairy\`               INT(6) UNSIGNED DEFAULT NULL;
+`;
+
 export const createTrainerHistoryTable = async (): Promise<void> => {
   logger.info('Creating leaderboard table if needed');
   await pool.execute(schemaCreationQuery);
@@ -110,4 +192,9 @@ export const createTrainerHistoryTable = async (): Promise<void> => {
 export const updateTrainerHistory = async (): Promise<void> => {
   logger.info('Updating trainer data');
   await pool.execute(schemaUpdateQuery);
+};
+
+export const migrateTrainerHistoryToGolbat = async (): Promise<void> => {
+  logger.info('Migrating leaderboard table to Golbat schema');
+  await pool.execute(migrateToGolbatQuery);
 };
