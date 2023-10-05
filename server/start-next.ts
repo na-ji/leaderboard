239d1f428aabce04f31ec7056fbe6b47881b7308
Naji Astier <naji.astier@algoan.com>
2023-10-05 21:21:54 +0200

    fix: fix server not starting

--- server/start-next.ts
+++ server/start-next.ts
@@ -1,6 +1,8 @@
 import { config } from 'node-config-ts';
 import { config as configureEnv } from 'dotenv';
-import cli from 'next/dist/cli/next-start.js';
+import { nextStart } from 'next/dist/cli/next-start.js';
+import { getValidatedArgs } from 'next/dist/lib/get-validated-args.js';
+import { commandArgs } from 'next/dist/lib/command-args.js';
 
 configureEnv();
-cli.nextStart(['-p', `${config.port || 3000}`]);
+nextStart(getValidatedArgs(commandArgs.start(), ['-p', `${config.port || 3000}`]));
